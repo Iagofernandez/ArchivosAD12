fdcce258785abf8c01254daed99d490a293b2006
oracle <oracle@localhost>
2019-10-15 11:30:24 +0200

    Ejercicio 12 1º) Se crea el FileOutputStreampara poder emplear el ObjectOutputStream 2º)Se crea una subclase con tres variables, una de ellas es transient(esta no se serializa como la sotras) 3º) Se crea un objeto MClase 4º) ObjectOutput usado para escribir en el archivo OutputStream 5º) Cerrados los flujos de Output y objectOut. 6º)Creado un nuevo objeto vacio, que mediante el método ObjectInput leerá el archivo serial.txt 7º) Se imprime lo que sale(todo sera igual salvo el dato de tipo transient que no se serializa)

--- src/serializacion1/Serializacion1.java
+++ src/serializacion1/Serializacion1.java
@@ -1,22 +1,25 @@
 
 package serializacion1;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Serializacion1 {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
 // Crear unha clase chamada mclase 
 // que implemente a interfaz serializable  (implements Serializable). 
 // Dita clase debe ter tres atributos  :
 // unha variable String  (nome)  outra int (numero1) e outra double (numero2) 
         MClase obx = new MClase("ola",-7,2.7E10);
         
-        
+        //Archivos de escritura
+       
         FileOutputStream escribir = new FileOutputStream("/home/oracle/Desktop/Eje11/serial.txt");
         ObjectOutputStream escribir1 = new ObjectOutputStream(escribir);
         
@@ -25,24 +28,45 @@ public class Serializacion1 {
         //todo excpeto las variables de tipo transient
         escribir1.writeObject(obx);
         
+        //cerrar los flujos de datos
+        escribir1.close();
+        escribir.close();
+        
+        MClase obx2= new MClase();
+        
+        FileInputStream leer = new FileInputStream("/home/oracle/Desktop/Eje11/serial.txt");
+        ObjectInputStream leer1 = new ObjectInputStream(leer);
+    
+        obx2 =(MClase) leer1.readObject();
+        leer1.close();
+        leer.close();
+        
+        
+        System.out.print(obx2);
+    
     }
+    
     
 }
 class MClase implements Serializable{
     private String serial;
-    
-    
     //Las variables tipo transient
     //demarcan el valor temporal de una variable
     //y indicamos que si el objeto que vamos a serializar, pues esta variable no lo hará
     private transient int nSerial;
     private double nSerial2;
 
+    public MClase() {
+    }
+
+    
     public MClase(String serial, int nSerial, double nSerial2) {
         this.serial = serial;
         this.nSerial = nSerial;
         this.nSerial2 = nSerial2;
     }
+
+    
 
     @Override
     public String toString() {
